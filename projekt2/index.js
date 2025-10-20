288de4e31c89162ea39184987cc7f84bb16fc2b2
Filip Wojciechowski <filipwojciechowski2806@gmail.com>
2025-10-20 07:11:01 +0000

    views

--- projekt2/index.js
+++ projekt2/index.js
@@ -1,9 +1,20 @@
 import express from "express";
 
 const port = 8000;
+const card_categories = ["j. angielski - food", "stolice europejskie"];
+
 const app = express();
 
-app.use(express.static("public")); // "magiczna" obsługa ścieżek dla plików z katalogu public
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/cards/categories/", (req, res) => {
+  res.render("categories", {
+    title: "Kategorie",
+    categories: card_categories,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
